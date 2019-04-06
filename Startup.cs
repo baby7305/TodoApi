08edc6e7ab997cf367077d0e87f8316879d779d7
baby7305 <songhaibei7305@163.com>
2019-04-06 15:16:49 +0800

    注册数据库上下文

--- Startup.cs
+++ Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TodoApi.Models;
 
 namespace TodoApi {
     public class Startup {
@@ -20,6 +22,8 @@ namespace TodoApi {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<TodoContext> (opt =>
+                opt.UseInMemoryDatabase ("TodoList"));
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
         }
 
